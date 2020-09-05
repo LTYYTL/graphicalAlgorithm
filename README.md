a1ef6880832581ba118e09706ff9fa37d208020a
LTYYTL <lzy991362541@163.com>
2020-09-05 11:22:25 +0800

    爬楼梯

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # graphicalAlgorithm
+* 数组（array）
+* 链表（linkedList）
+* 动态规划（dp）
---
+++ src/com/test/dp/ClimbStairsTest.java
@@ -0,0 +1,10 @@
+package com.test.dp;
+
+import com.dp.ClimbStairs;
+
+public class ClimbStairsTest {
+    public static void main(String[] args) {
+        ClimbStairs climbStairs = new ClimbStairs();
+        System.out.println("有"+climbStairs.climbStairs(3)+"种方式");
+    }
+}
