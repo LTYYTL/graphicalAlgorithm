606854718df917eb386d1066fb7ca531b7c6b2b9
LTYYTL <lzy991362541@163.com>
2020-09-18 19:15:39 +0800

    二叉树的最大深度

---
+++ src/com/binaryTree/TreeNode.java
@@ -0,0 +1,47 @@
+package com.binaryTree;
+
+public class TreeNode<E> {
+    public TreeNode root;
+    public E val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(E e) {
+        val = e;
+    }
+
+    /**
+     * 通过数组构建树
+     * @param nums
+     */
+    public TreeNode(E[] nums){
+        root = createBinaryTree(nums,0);
+    }
+
+    /**
+     * 私有方法
+     * @param array 数组
+     * @param index 节点的位置
+     * @return
+     */
+    private TreeNode createBinaryTree(E[] array, int index) {
+        TreeNode tn = null;
+        if (index < array.length){
+            E value = array[index];
+            tn = new TreeNode(value);
+            tn.left = createBinaryTree(array, 2*index+1);
+            tn.right = createBinaryTree(array, 2*index+2);
+            return tn;
+        }
+        return tn;
+    }
+
+    @Override
+    public String toString() {
+        return "TreeNode{" +
+                "val=" + val +
+                ", left=" + left +
+                ", right=" + right +
+                '}';
+    }
+}
